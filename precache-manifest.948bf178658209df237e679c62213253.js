d876abb1040a57e6942ad48ee8814c3b9d4bf794
Ganesh <gkangane47@gmail.com>
2019-11-30 16:13:47 +0530

    Updates

---
+++ precache-manifest.948bf178658209df237e679c62213253.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dcde785a16d58c6124ac1da11b48c6c",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "d3374d02d425d3da3c2e",
+    "url": "/react-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "32787f121ad857d26878",
+    "url": "/react-app/static/js/2.4be9cd84.chunk.js"
+  },
+  {
+    "revision": "d3374d02d425d3da3c2e",
+    "url": "/react-app/static/js/main.67892fc0.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  }
+]);
